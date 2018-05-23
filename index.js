79598537bf7203fceacee0f57fe0055fdb320e3d
Justin Hall <hall.justin.d@gmail.com>
2018-05-23 20:53:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,6 +23,6 @@ function congratulateCustomer(){
 }
 
 function attemptTwoFavoriteCustomers(){
-    let favoriteCustomer = 'bar'
-    let favoriteCustomer = 'not bar'
+    var favoriteCustomer = 'bar'
+    var favoriteCustomer = 'not bar'
 }
